29742bc0d6e73c859ed9f0d3e3dda85cfc1bf960
Johan van den Dorpe <johan@johan.org.uk>
2014-04-21 13:34:18 +0100

    Add basis for event filtering

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,14 @@ App.IndexRoute = Ember.Route.extend({
       cache: false,
       dataType: 'json',
       url: '/api/services'
-    })
+    }).done(function(services) {
+      $.each(services, function(id,service) {
+        // get number of raid_engenio1 checks
+        service["priority"] = service["allevents"].filter(function(obj) { 
+          return (obj.check === "raid_engenio1");
+        }).length;
+        console.log(service["priority"]);
+      });
+    });
   }
 });
